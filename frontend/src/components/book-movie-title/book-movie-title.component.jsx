7206ae4adef27c3598a1d7dccfefc695b5762c57
adi@778036 <rayaadinarayana444@gmail>
2022-12-26 12:03:47 +0530

    updated

--- frontend/src/components/book-movie-title/book-movie-title.component.jsx
+++ frontend/src/components/book-movie-title/book-movie-title.component.jsx
@@ -8,12 +8,12 @@ const BookMovieTitle = ({ match }) => {
         <div className='date-list-container'>
             <h1>Select the Date from the below mentioned:</h1>
             <div className='date-list'>
-                <Link to={`${match.url}/01-11-2019`}><span>01-11-2019</span></Link>
-                <Link to={`${match.url}/03-11-2019`}><span>03-11-2019</span></Link>
-                <Link to={`${match.url}/07-11-2019`}><span>07-11-2019</span></Link>
-                <Link to={`${match.url}/10-11-2019`}><span>10-11-2019</span></Link>
-                <Link to={`${match.url}/16-11-2019`}><span>16-11-2019</span></Link>
-                <Link to={`${match.url}/21-11-2019`}><span>21-11-2019</span></Link>
+                <Link to={`${match.url}/06-12-2022`}><span>06-12-2022</span></Link>
+                <Link to={`${match.url}/11-12-2022`}><span>11-12-2022</span></Link>
+                <Link to={`${match.url}/18-12-2022`}><span>18-12-2022</span></Link>
+                <Link to={`${match.url}/23-12-2022`}><span>23-12-2022</span></Link>
+                <Link to={`${match.url}/26-12-2022`}><span>26-12-2022</span></Link>
+                <Link to={`${match.url}/31-12-2022`}><span>31-12-2022</span></Link>
             </div>
         </div>
     );
